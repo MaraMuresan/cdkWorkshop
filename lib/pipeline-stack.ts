98806a349e93e9d9805e5fa88de1e0dee658094d
Mara Muresan <maramure@amazon.de>
2025-07-08 10:29:35 +0200

    Add deploy stage to pipeline

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,16 +1,25 @@
+import * as cdk from "aws-cdk-lib";
 import { Stack, StackProps } from "aws-cdk-lib";
 import { CodeBuildStep, CodePipeline, CodePipelineSource } from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
+import { WorkshopPipelineStage } from "./pipeline-stage";
 
 export class WorkshopPipelineStack extends Stack {
     constructor (scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
         // Creates a GitHub repository source
-        const repo = CodePipelineSource.gitHub('MaraMuresan/cdkWorkshop', 'main');
+        const repo = CodePipelineSource.gitHub('MaraMuresan/cdkWorkshop', 'main', {
+            authentication: cdk.SecretValue.secretsManager('github-token')
+        });
 
         // The basic pipeline declaration. This sets the initial structure
         // of the pipeline
+        //on CodePipeline Console I see 3 green boxes:
+        // 1. Source: Pipeline pulled code from GitHub repo
+        // 2. Build: Pipeline ran the synth commands: npm ci, npm run build, npx cdk synth
+        // 3. UpdatePipeline: Pipeline updated itself
+        // Eveytime I git push to the GitHub repo, the pipeline will automatically trigger
         const pipeline = new CodePipeline(this, "Pipeline", {
             pipelineName: "WorkshopPipeline",
             synth: new CodeBuildStep("SynthStep", {
@@ -19,8 +28,13 @@ export class WorkshopPipelineStack extends Stack {
                     "npm ci",
                     "npm run build",
                     "npx cdk synth"
+                    // these commands prepare the CDK code to be deployed by converting it from
+                    // TypeScript -> JavaScript -> CloudFormation
                 ]
-            })
-        })
+            }),
+        });
+
+        const deploy = new WorkshopPipelineStage(this, "Deploy");
+        const deployStage = pipeline.addStage(deploy);
     }
 }
---
+++ lib/pipeline-stage.ts
@@ -0,0 +1,11 @@
+import { CdkintroStack} from "./cdkintro-stack";
+import { Stage, StageProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+export class WorkshopPipelineStage extends Stage {
+    constructor(scope: Construct, id: string, props?: StageProps) {
+        super(scope, id, props);
+
+        const service = new CdkintroStack(this, "WebService");
+    }
+}
