42a3f5b35fa8217f6d6144b22de7ac8dddfa1de2
Mara Muresan <maramure@amazon.de>
2025-07-08 13:34:50 +0200

    Add validation tests

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -38,5 +38,31 @@ export class WorkshopPipelineStack extends Stack {
 
         const deploy = new WorkshopPipelineStage(this, "Deploy");
         const deployStage = pipeline.addStage(deploy);
+
+        // addPost = add post-deployment steps that run
+        // after the application is deployed
+        deployStage.addPost(
+            new CodeBuildStep('TestViewerEndpoint', {
+                projectName: 'TestViewerEndpoint',
+                envFromCfnOutputs: {
+                    ENPOINT_URL: deploy.hcViewUrl,
+                },
+                commands: [
+                    'curl -Ssf $ENDPOINT_URL'
+                ]
+            }),
+
+            new CodeBuildStep('TestAPIGatewayEndpoint', {
+                projectName: 'TestAPIGatewayEndpoint',
+                envFromCfnOutputs: {
+                    ENDPOINT_URL: deploy.hcEndpoint,
+                },
+                commands: [
+                    'curl -Ssf $ENDPOINT_URL',
+                    'curl -Ssf $ENDPOINT_URL/hello',
+                    'curl -Ssf $ENDPOINT_URL/test'
+                ]
+            })
+        )
     }
 }
--- lib/pipeline-stage.ts
+++ lib/pipeline-stage.ts
@@ -1,11 +1,18 @@
 import { CdkintroStack} from "./cdkintro-stack";
-import { Stage, StageProps } from 'aws-cdk-lib';
+import { Stage, CfnOutput, StageProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 export class WorkshopPipelineStage extends Stage {
+
+    public readonly hcViewUrl: CfnOutput;
+    public readonly hcEndpoint: CfnOutput;
+
     constructor(scope: Construct, id: string, props?: StageProps) {
         super(scope, id, props);
 
         const service = new CdkintroStack(this, "WebService");
+
+        this.hcEndpoint = service.hcEndpoint;
+        this.hcViewUrl = service.hcViewerUrl;
     }
 }
